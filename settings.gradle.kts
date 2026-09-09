2756f725c1295357e0216ba9333bb2bf89ee8181
hermes_Tangtang <hermes@tangtang>
2026-09-09 21:44:01 +0800

    ci: add GitHub Actions build, add WebRTC repo, remove init.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://storage.googleapis.com/webrtc-builds/maven/") }
     }
 }
 
