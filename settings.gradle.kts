5a4265eb5b75971fa893b347becbfb87fc91862c
hermes_Tangtang <hermes@tangtang>
2026-09-09 22:46:08 +0800

    fix: upgrade AGP to 8.4.0, Kotlin to 1.9.23, remove WebRTC repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://storage.googleapis.com/webrtc-builds/maven/") }
     }
 }
 
