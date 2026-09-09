fe5328d1772f68e8d6e46f05b2515b0a322c7e6b
luoxiaoliangTj <76738994+luoxiaoliangTj@users.noreply.github.com>
2026-09-10 07:15:56 +0800

    fix: add missing android.view.View import

--- app/src/main/java/com/example/walkie/MainActivity.kt
+++ app/src/main/java/com/example/walkie/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.IBinder
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
